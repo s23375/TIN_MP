6e0fff3580afc101c1b1a130266e46465902c822
Bartek Janowski <s23375@pjwstk.edu.pl>
2022-11-30 00:08:36 +0100

    Exercise 4/5 - form.ejs for ProductModel doesn't work(waiting for the teacher's answer), changes in productModelController, productModel list.ejs displaying info from database

--- nodejs/tin/controllers/productModelController.js
+++ nodejs/tin/controllers/productModelController.js
@@ -1,15 +1,52 @@
+const ProductModelRepository = require('../repository/sequelize/ProductModelRepository');
+
 exports.showProductModelList = (req, res, next) => {
-    res.render('pages/ProductModel/list', { navLocation: 'productModel' });
+    ProductModelRepository.getProducts()
+        .then(products => {
+            res.render('pages/ProductModel/list', {
+                products: products, //products is the passed name we will use in the .ejs template
+                navLocation: "productModel"
+            });
+        });
 }
 
 exports.showAddProductModelForm = (req, res, next) => {
-    res.render('pages/ProductModel/form', { navLocation: 'productModel' });
+    res.render('pages/ProductModel/form', {
+        product: {},
+        pageTitle: "New productModel",
+        formMode: "createNew",
+        btnLabel: "Add productModel",
+        formAction: "/ProductModel/add",
+        navLocation: "productModel"
+    });
 }
 
 exports.showProductModelDetails = (req, res, next) => {
-    res.render('pages/ProductModel/form-detail', { navLocation: 'productModel' });
+    const productID = req.params.IDproduct;
+    ProductModelRepository.getProductById(productID)
+        .then(product => {
+            res.render('pages/ProductModel/form-detail', {
+                product: product,
+                pageTitle: "Details of productModel",
+                formMode: "showDetails",
+                //btnLabel: "Details of productModel",
+                formAction: "",
+                navLocation: "productModel"
+            });
+        });
 }
 
 exports.showProductModelEdit = (req, res, next) => {
-    res.render('pages/ProductModel/form-edit', { navLocation: 'productModel' });
-}
+    const productID = req.params.IDproduct;
+    ProductModelRepository.getProductById(productID)
+        .then(product => {
+            res.render('pages/ProductModel/form-edit', {
+                product: product,
+                pageTitle: "Edit productModel",
+                formMode: "edit",
+                btnLabel: "Edit productModel",
+                formAction: "/ProductModel/edit",
+                navLocation: "productModel"
+            });
+        });
+};
