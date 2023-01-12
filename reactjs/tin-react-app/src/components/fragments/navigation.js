8daccd081d8a26f5f0c8860ac50470b3ff278f26
Bartek Janowski <s23375@pjwstk.edu.pl>
2023-01-12 05:16:46 +0100

    MP3 React.js (3/3) - language buttons and css for those buttons

--- reactjs/tin-react-app/src/components/fragments/navigation.js
+++ reactjs/tin-react-app/src/components/fragments/navigation.js
@@ -1,17 +1,35 @@
 import React from "react"
 import {Link} from "react-router-dom"
+import { withTranslation } from "react-i18next";
 
-function Navigation() {
-    return (
-        <nav>
-            <ul>
-                <li><Link to="/">Main page</Link></li>
-                <li><Link to="/ProductModel">Product models</Link></li>
-                <li><Link to="/Order">Orders</Link></li>
-                <li><Link to="/OrderedProducts">Ordered products</Link></li>
-            </ul>
-        </nav>
-    )
+
+
+class Navigation extends React.Component{
+
+    handleLanguageChange = (language) => {
+        const { i18n } = this.props
+        i18n.changeLanguage(language, (err, t) => {
+            if(err) return console.log("Something went wrong when loading", err)
+        });
+    }
+
+    render() {
+        return (
+            <nav>
+                <ul>
+                    <li><Link to="/">Main page</Link></li>
+                    <li><Link to="/ProductModel">Product models</Link></li>
+                    <li><Link to="/Order">Orders</Link></li>
+                    <li><Link to="/OrderedProducts">Ordered products</Link></li>
+                </ul>
+                <ul>
+                    <li className="lang"><button onClick={() => {this.handleLanguageChange("en") }}>EN</button></li>
+                    <li className="lang"><button onClick={() => {this.handleLanguageChange("pl") }}>PL</button></li>
+                </ul>
+            </nav>
+        )
+    }
+
 }
 
-export default Navigation;
+export default withTranslation()(Navigation);
