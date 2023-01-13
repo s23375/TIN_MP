0acdee6da823e4e98153649c9710e770a74572d8
Bartek Janowski <s23375@pjwstk.edu.pl>
2023-01-13 22:41:18 +0100

    MP3 React.js (3/3) - translated ProductModel errors

--- reactjs/tin-react-app/src/helpers/formHelper.js
+++ reactjs/tin-react-app/src/helpers/formHelper.js
@@ -3,4 +3,25 @@ const formMode = {
     EDIT: "EDIT"
 }
 
+export const formValidationKeys = {
+    notEmpty: "notEmpty",
+    isInt: "isInt",
+    isUniqueName: "isUniqueName",
+    isAlpha: "isAlpha",
+    len_2_60: "len_2_60",
+    len_1_11: "len_1_11",
+    isDecimal: "isDecimal",
+    isPositive: "isPositive",
+    isDate: "isDate",
+    endDistributionDate: "endDistributionDate"
+}
+
+export function getValidationErrorKey(error) {
+    if(error) {
+        return `validation.messages.${error}`
+    } else {
+        return ""
+    }
+}
+
 export default formMode
