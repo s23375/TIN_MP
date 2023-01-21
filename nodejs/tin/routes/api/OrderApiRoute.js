adf522fabfb46963a420a0b5ce6767fc56df20e9
Bartek Janowski <s23375@pjwstk.edu.pl>
2023-01-21 05:47:25 +0100

    Nodejs MP3 - added authorization required server-side

--- nodejs/tin/routes/api/OrderApiRoute.js
+++ nodejs/tin/routes/api/OrderApiRoute.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const orderApiController = require('../../api/OrderAPI');
+const isAuth = require("../../middleware/isAuth");
 
-router.get('/', orderApiController.getOrders); //TODO VERY IMPORTANT LESSON: THE NAME YOU GIVE FOR COMING ID HERE WILL BE USED TO REFER TO IT IN orderApiController
-router.get('/:IDorder', orderApiController.getOrderById);
-router.post('/', orderApiController.createOrder);
-router.put('/:IDorder', orderApiController.updateProduct);
-router.delete('/:IDorder', orderApiController.deleteOrder);
+router.get('/', isAuth, orderApiController.getOrders); //TODO VERY IMPORTANT LESSON: THE NAME YOU GIVE FOR COMING ID HERE WILL BE USED TO REFER TO IT IN orderApiController
+router.get('/:IDorder', isAuth, orderApiController.getOrderById);
+router.post('/', isAuth, orderApiController.createOrder);
+router.put('/:IDorder', isAuth, orderApiController.updateProduct);
+router.delete('/:IDorder', isAuth, orderApiController.deleteOrder);
 
 module.exports = router;
